c3fe1b493d8d663a68e44c63b186df3bddbef36b
Daniel50726 <62769266+Daniel50726@users.noreply.github.com>
2020-07-08 15:56:57 -0500

    Fondo Parte 2
    Añadimos obtaculos para los dos niveles

--- Juego/VideoGame/VideoGame.pro
+++ Juego/VideoGame/VideoGame.pro
@@ -17,10 +17,12 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
     main.cpp \
-    mainwindow.cpp
+    mainwindow.cpp \
+    obstaculos.cpp
 
 HEADERS += \
-    mainwindow.h
+    mainwindow.h \
+    obstaculos.h
 
 FORMS += \
     mainwindow.ui
--- Juego/VideoGame/mainwindow.h
+++ Juego/VideoGame/mainwindow.h
@@ -7,6 +7,7 @@
 #include <QWidget>
 #include <QMainWindow>
 #include <QGraphicsScene>
+#include "obstaculos.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -21,9 +22,12 @@ public:
     ~MainWindow();
     QGraphicsScene *scene;
     void niveles();
+    void colision();
 
 private:
     int x=0;
     Ui::MainWindow *ui;
+    QList <obstaculos*> lista_piedra;
+    obstaculos *o1,*o2,*o3,*o4,*o5,*o6,*o7,*o8,*o9,*o10,*o11,*o12,*o13,*o14,*o15,*o16,*o17,*o18,*o19,*o20,*o21,*o22,*o23,*o24,*o25;
 };
 #endif // MAINWINDOW_H
